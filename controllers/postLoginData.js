c4454a9b7b00c681b403b0bc29195e6f6dd13f1e
Evanka Dsouza <64545413+evankadsouza@users.noreply.github.com>
2024-04-18 12:14:53 +0530

    Update postLoginData.js

--- controllers/postLoginData.js
+++ controllers/postLoginData.js
@@ -6,7 +6,7 @@ const postLoginData ={
     postLogin: async (req, res) => {
   const { userName, loginPIN } = req.body;
   try {
-    const query = `SELECT * FROM USERTABLE WHERE userName = '${userName}' AND loginPIN = ${loginPIN}`;
+    const query = `SELECT * FROM userTable WHERE userName = '${userName}' AND loginPIN = ${loginPIN}`;
     const [result] = await connection.query(query);
     if(result[0]){
         return res.status(200).json({ "result":result});
